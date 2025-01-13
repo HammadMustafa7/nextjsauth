28ba5851723f0596d4599d349ab68dba87908cbf
Hammad Mustafa <hammadmustafa8401@gmail.com>
2025-01-13 11:07:56 +0500

    change domain

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -37,7 +37,7 @@ export const sendEmail = async ({email, emailType, userId} :any ) => {
                 to: email,  
                 subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
                 html: `
-                <p>Click "${userId}" <a href="${process.env.DOMAIN}/${emailType === "VERIFY" ? "verifyemail" : "forgetpassword"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+                <p>Click "${userId}" <a href="https://nextjsauth-ashen.vercel.app/${emailType === "VERIFY" ? "verifyemail" : "forgetpassword"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
                 or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
                 </p>
                 
